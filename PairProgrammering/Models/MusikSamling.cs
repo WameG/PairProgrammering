d2984467cc1cb39c65fa0ad37caa74d7292d480f
WameG <wgassama@yahoo.dk>
2023-03-20 14:35:54 +0100

    Add project files.

---
+++ PairProgrammering/Models/MusikSamling.cs
@@ -0,0 +1,10 @@
+﻿namespace PairProgrammering.Models
+{
+    public class MusikSamling
+    {
+        public string Titel { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public string PublicationYear { get; set; }
+    }
+}
---
+++ PairProgrammering/Repository/MusikSamlingRepository.cs
@@ -0,0 +1,20 @@
+﻿using PairProgrammering.Models;
+
+namespace PairProgrammering.Repository
+{
+    public class MusikSamlingRepository
+    {
+        public List<MusikSamling> musikSamlings;
+
+        public MusikSamlingRepository()
+        {
+            musikSamlings = new List<MusikSamling>()
+            {
+            new MusikSamling() { Titel = "Get Low", Artist = "Jan", Duration = 3, PublicationYear = "2021" },
+            new MusikSamling() { Titel = "Gunna", Artist = "Josh", Duration = 3, PublicationYear = "2022" },
+            new MusikSamling() { Titel = "Don't push me", Artist = "John", Duration = 3, PublicationYear = "2023" },
+            };
+
+        }
+    }
+}
