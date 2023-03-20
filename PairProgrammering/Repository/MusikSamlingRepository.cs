2ce66bfe56f06db94682097e75db8315173c96ad
ericc <ericc@LAPTOP-97HVV20R>
2023-03-20 18:28:55 +0100

    commit

--- PairProgrammering/Repository/MusikSamlingRepository.cs
+++ PairProgrammering/Repository/MusikSamlingRepository.cs
@@ -1,4 +1,5 @@
 ﻿using PairProgrammering.Models;
+using System.Security.Cryptography.Xml;
 
 namespace PairProgrammering.Repository
 {
@@ -14,6 +15,27 @@ namespace PairProgrammering.Repository
             new MusikSamling() { Titel = "Gunna", Artist = "Josh", Duration = 3, PublicationYear = "2022" },
             new MusikSamling() { Titel = "Don't push me", Artist = "John", Duration = 3, PublicationYear = "2023" },
             };
+
+        }
+
+        public List<MusikSamling> Get()
+        {
+         
+            return musikSamlings; 
+
+        }
+
+        public List<MusikSamling> GetFiltered(string title = null, string sortBy= null)
+        {
+
+            list<musikSamlings> 
+            if(title != null)
+            {
+                musikSamlings.FindAll(x => x.Titel == title).or
+            }
+
+            
+
 
         }
     }
